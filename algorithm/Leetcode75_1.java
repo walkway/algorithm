bb8985f4e02aba3000f976f72321798f24454e4c
walkway <jinhee.lamp@gmail.com>
2021-10-09 18:11:08 +0900

    feat: LeetCode 8 문제 풀이 추가

--- algorithm/Leetcode75_1.java
+++ algorithm/Leetcode75_1.java
@@ -6,15 +6,16 @@ import java.io.IOException;
  */
 class Leetcode75_1 {
     public static void main(String args[]) throws IOException {
-        sortColors(new int[] {2,0,2,1,1,0});
+        sortColors(new int[]{2, 0, 2, 1, 1, 0});
     }
+
     public static void sortColors(int[] nums) {
-        for(int i = 0 ; i < nums.length - 1 ; i ++) {
-            for(int j = 0 ; j < nums.length - i - 1 ; j ++) {
-                if(nums[j] > nums[j+1]) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
                     int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
                 }
             }
         }
