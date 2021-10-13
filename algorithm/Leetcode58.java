b8b05914fa617b912b1bda5a11f15520a76c32e9
walkway <jinhee.lamp@gmail.com>
2021-10-13 20:37:42 +0900

    feat: LeetCode 58 문제 풀이

---
+++ algorithm/Leetcode58.java
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.com/problems/length-of-last-word/
+ * Length of Last Word
+ */
+class Leetcode58 {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            } else {
+                if (length > 0) return length;
+            }
+        }
+        return length;
+    }
+}
