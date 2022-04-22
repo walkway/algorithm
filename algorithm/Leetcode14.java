95e53e914328e28bd704db31b6da5da4d4db99d1
walkway <jinhee.lamp@gmail.com>
2022-04-22 21:13:19 +0900

    feat: Leetcode 14

---
+++ algorithm/Leetcode14.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/longest-common-prefix/
+ * Longest Common Prefix
+ */
+class Leetcode14 {
+
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String start = strs[0];
+        String end = strs[strs.length - 1];
+        int i = 0;
+        while (i < start.length()) {
+            if (start.charAt(i) == end.charAt(i)) {
+                i++;
+            } else {
+                break;
+            }
+        }
+
+        return i == 0 ? "" : start.substring(0, i);
+    }
+}
