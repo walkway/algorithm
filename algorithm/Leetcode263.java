7c1bef2f7d7c0255b10a9aa631e6a559d80c6139
walkway <jinhee.lamp@gmail.com>
2022-07-20 16:29:19 +0900

    feat: LeetCode 263

---
+++ algorithm/Leetcode263.java
@@ -0,0 +1,27 @@
+/**
+ * https://leetcode.com/problems/ugly-number/
+ * Ugly Number
+ */
+class Leetcode263 {
+
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+
+        return num == 1;
+    }
+
+}
