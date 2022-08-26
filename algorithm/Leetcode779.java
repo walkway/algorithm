e0f8bbc46ccd8e83f9b23d8ea778383b3a072f22
walkway <jinhee.lamp@gmail.com>
2022-08-26 17:34:52 +0900

    feat: LeetCode 779

---
+++ algorithm/Leetcode779.java
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.com/problems/k-th-symbol-in-grammar/
+ * K-th Symbol in Grammar
+ */
+class Leetcode779 {
+    public int kthGrammar(int n, int k) {
+
+        if (n == 1 && k == 1) {
+            return 0;
+        }
+
+        if (n == 2) {
+            return (k == 1) ? 0 : 1;
+        }
+
+        int mid = (int) Math.pow(2, n - 1) / 2;
+        if (k <= mid) {
+            return kthGrammar(n - 1, k);
+        } else {
+            return (kthGrammar(n - 1, k - mid) == 1) ? 0 : 1;
+        }
+    }
+}
