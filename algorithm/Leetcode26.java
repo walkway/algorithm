f5ac50e9f368e0dfd8b3bd07d0e28da805cd0c4c
walkway <jinhee.lamp@gmail.com>
2022-08-27 14:15:28 +0900

    feat: Leetcode 26

---
+++ algorithm/Leetcode26.java
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ * Remove Duplicates from Sorted Array
+ */
+class Leetcode26 {
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[] {0,0,1,1,1,2,2,3,3,4}));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int index = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[index] != nums[i]) {
+                index++;
+                nums[index] = nums[i];
+            }
+        }
+
+        return index + 1;
+    }
+}
