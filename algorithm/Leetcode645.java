1e8f9205b7b52b43779a3f8c7cb80aa948fcce38
walkway <jinhee.lamp@gmail.com>
2022-09-17 15:44:03 +0900

    update: Leetcode 645

--- algorithm/Leetcode645.java
+++ algorithm/Leetcode645.java
@@ -1,24 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/set-mismatch/
  * Set Mismatch
  */
 class Leetcode645 {
 
-    public static int[] findErrorNums(int[] nums) {
+    public int[] findErrorNums(int[] nums) {
+        Set<Integer> numbers = new HashSet<>();
         int[] result = new int[2];
-        for (int i : nums) {
-            if (nums[Math.abs(i) - 1] < 0) {
-                result[0] = Math.abs(i);
+        for (int n : nums) {
+            if (numbers.contains(n)) {
+                result[0] = n;
             } else {
-                nums[Math.abs(i) - 1] *= -1;
+                numbers.add(n);
             }
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                result[1] = i + 1;
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (!numbers.contains(i)) {
+                result[1] = i;
             }
         }
 
         return result;
     }
+
 }
